110aa9f277f32fa8da436f7dd858371c263f5cdc
Islam Mohammed <islammohammed1978@gmail.com>
2022-12-25 01:43:10 +0200

    Handling validation errors

--- auth/src/routes/routes.ts
+++ auth/src/routes/routes.ts
@@ -1,15 +1,32 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const authRouter = express.Router();
 
 authRouter.get("/current", (req, res) => {
-  console.log("current");
   return res.send("Get Crurrent User!");
 });
-authRouter.post("/create", (req, res) => {
-  return res.send("Create New User!");
-});
+authRouter.post(
+  "/signup",
+  [
+    body("email").isEmail().withMessage("Email is not valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 10 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+    const { email, password } = req.body;
+    return res.send("Create New User!");
+  }
+);
 authRouter.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+
   return res.send("Handle user sign in!");
 });
 authRouter.post("/signout", (req, res) => {
