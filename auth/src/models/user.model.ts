f5bb50eb8897c1ede66a2b24d624bec47220be27
Islam Mohammed <islammohammed1978@gmail.com>
2022-12-25 20:09:07 +0200

    Create user schema and user model

---
+++ auth/src/models/user.model.ts
@@ -0,0 +1,19 @@
+import { model, Schema, Model, Document } from "mongoose";
+
+export interface IUser extends Document {
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+}
+
+const UserSchema: Schema = new Schema({
+  email: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const User = model<IUser>("User", UserSchema);
+
+export default User;
