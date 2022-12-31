740a6e4efd52ba6fb0d123d1d3da48ce56e00bb7
Islam Mohammed <islammohammed1978@gmail.com>
2022-12-31 16:05:17 +0200

    Client | Load current user data from the server and cache it

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,15 +1,39 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
 import Layout from "../components/layout/layout";
+import { GetServerSideProps } from "next";
+import axiosServer from "../helpers/axios.helper";
+import { useSWRConfig } from "swr";
+import { useEffect } from "react";
+import getSWRCacheKey from "../helpers/swr.helper";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+interface Props {
+  user?: string;
+}
+
+const Home = ({ user }: Props) => {
+  const { mutate } = useSWRConfig();
+  useEffect(() => {
+    const updateUser = async () => {
+      await mutate(getSWRCacheKey().user, user);
+    };
+    updateUser();
+  }, [user]);
   return (
     <Layout>
       <h1>Home Page</h1>
     </Layout>
   );
-}
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const { data: user } = await axiosServer(req).get("/api/users/current");
+  return {
+    props: {
+      user,
+    },
+  };
+};
+
+export default Home;
