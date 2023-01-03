989bc4d48c7d0a7f0cd5a86457bf9fef0f35d159
Islam Mohammed <islammohammed1978@gmail.com>
2023-01-03 10:45:14 +0200

    Tickets | Add cookie session middleware

--- tickets/app.ts
+++ tickets/app.ts
@@ -1,21 +1,30 @@
+import cookieSession from "cookie-session";
 import express from "express";
 import "express-async-errors";
 
 import { json } from "body-parser";
 import helmet from "helmet";
-import { errorHandler, NotFoundError } from "@stubhubdev/common";
+import { currentUser, errorHandler, NotFoundError } from "@stubhubdev/common";
+import router from "./src/routes/routes";
 
 const app = express();
 app.use(json());
 app.use(helmet());
 app.set("trust proxy", true); // trust ingress (broxy)
+app.use(
+  cookieSession({
+    signed: false, // disable encryption
+    secure: process.env.NODE_ENV === "test" ? false : true, // used only with secured connection
+  })
+);
 
-// app.use("/api/tickets");
+app.use(currentUser);
+
+app.use("/api/tickets", router);
 
 app.get("/*", (req, res) => {
   throw new NotFoundError();
 });
 
 app.use(errorHandler);
-console.log("should be working");
 export default app;
