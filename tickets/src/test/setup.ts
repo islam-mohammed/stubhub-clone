0dff89078bfb68c8514e6982c0f2770fbdf58090
Islam Mohammed <islammohammed1978@gmail.com>
2023-01-03 07:04:01 +0200

    Tickets |  Manully create cookie for testing

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,6 +1,11 @@
 import request from "supertest";
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
+import { sign } from "jsonwebtoken";
+
+declare global {
+  var getAuthCookie: () => Promise<string[]>;
+}
 
 let mongo: MongoMemoryServer;
 beforeAll(async () => {
@@ -20,3 +25,18 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.getAuthCookie = async () => {
+  const jwt = sign(
+    {
+      email: "test@test.com",
+      password: "newPa$$0rd",
+      firstName: "fname",
+      lastName: "lname",
+    },
+    process.env.JWT_KEY!
+  );
+
+  const session = JSON.stringify({ jwt });
+  return [`express:sess=${Buffer.from(session).toString("base64")}`];
+};
